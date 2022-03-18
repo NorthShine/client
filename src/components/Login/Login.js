4431b2b5d5b616213f5851c66448ce9daa68996f
Paul <52328887+paulbraam@users.noreply.github.com>
2022-03-18 19:16:28 +0300

    Updated login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,73 +1,55 @@
 import { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Grid, Typography, Container } from '@mui/material';
+import { Button, Grid, Typography, Container, Tabs, Tab, Box } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
 import TextField from '@mui/material/TextField';
 import { useNotification } from '../../hooks/useNotification';
 import { useAuth } from '../../hooks/useAuth';
-import { PasswordInput } from '../PasswordInput/PasswordInput';
 import styles from './Login.module.css';
 
 export const Login = () => {
   const navigate = useNavigate();
   const { setAuth, isAuth } = useAuth();
-  const [email, setEmail] = useState({
-    name: 'email',
-    inputRef: useRef(null),
-    value: '',
-    checked: false,
-    exists: false
-  });
-  const [password, setPassword] = useState({
-    name: 'password',
-    inputRef: useRef(null),
-    error: false
-  });
-  const [passwordRepeat, setPasswordRepeat] = useState({
-    name: 'password_repeat',
-    inputRef: useRef(null),
-    error: false
-  });
+  const emailRef = useRef(null);
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   useEffect(() => {
     if (isAuth) navigate('/');
     // eslint-disable-next-line
   }, []);
 
-  const getValue = input => input.inputRef.current?.value ?? '';
-
   const handleSubmit = async e => {
     e.preventDefault();
     // signin logic
-  };
-
-  const handleChange = () => {
-    const isEqual =
-      (password.error || passwordRepeat.error) && getValue(password) === getValue(passwordRepeat);
-    const passwordErrorCallback = prev => ({ ...prev, error: isEqual });
-    setPassword(passwordErrorCallback);
-    setPasswordRepeat(passwordErrorCallback);
   };
 
   return (
     <Container component="main" className={styles.loginBox} maxWidth="xs">
       <Typography variant="h5">Signup / Login</Typography>
       <form className={styles.form} onSubmit={handleSubmit}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Item One" value="1" />
+              <Tab label="Item Two" value="2" />
+            </TabList>
+          </Box>
+        </TabContext>
         <Grid container>
           <TextField
-            label="Email Address"
-            id={email.name}
             className={styles.input}
+            label="Email Address"
+            id="email"
             variant="outlined"
             type="email"
-            inputRef={email.inputRef}
-            onChange={handleChange}
+            inputRef={emailRef}
             fullWidth
             autoFocus
           />
-          <PasswordInput onChange={handleChange} {...password} />
-          {email.checked && !email.exists && (
-            <PasswordInput onChange={handleChange} {...passwordRepeat} />
-          )}
           <Button
             type="submit"
             fullWidth
