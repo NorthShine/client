50c1a02bb94e437d6a066feb8bb31c7c6564299b
Paul <52328887+paulbraam@users.noreply.github.com>
2022-03-18 15:21:32 +0300

    Revert "Initial commit"
    This reverts commit 8ec795451469135458b2952090dae20a7b013033.

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:14.18.2 as build
-WORKDIR /app
-COPY package.json .
-RUN npm i
-COPY . .
-RUN npm run build
--- src/routing/Routes.js
+++
@@ -1,7 +0,0 @@
-import { useRoutes } from 'react-router-dom';
-import { routes } from './config';
-
-export const Routes = () => {
-  const routing = useRoutes(routes);
-  return routing;
-};
